19acf43fc6e554fc50365ed0205c2adb10fb1eee
Ossu-V <oscarvalk7@gmail.com>
2026-09-21 14:13:12 +0300

    DetailsViewAdded/AddedUpdateView/ViewModel

--- ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
+++ ShopTARpe25/ShopTARpe25/Controllers/SpaceshipController.cs
@@ -107,5 +107,54 @@ namespace ShopTARpe25.Controllers
 
             return View(vm);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var spaceship = await _spaceshipService.DetailsAsync(id);
+
+            if (spaceship == null)
+            {
+                return NotFound();
+            }
+
+            var vm = new SpaceshipUpdateViewModel();
+
+            vm.Id = spaceship.Id;
+            vm.Name = spaceship.Name;
+            vm.Classification = spaceship.Classification;
+            vm.BuiltDate = spaceship.BuiltDate;
+            vm.Crew = spaceship.Crew;
+            vm.EnginePower = spaceship.EnginePower;
+            vm.CreatedAt = spaceship.CreatedAt;
+            vm.ModifiedAt = spaceship.ModifiedAt;
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(SpaceshipUpdateViewModel vm)
+        {
+            var dto = new SpaceshipDto()
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                Classification = vm.Classification,
+                BuiltDate = vm.BuiltDate,
+                Crew = vm.Crew,
+                EnginePower = vm.EnginePower,
+                CreatedAt = vm.CreatedAt,
+                ModifiedAt = vm.ModifiedAt
+            };
+
+            var result = await _spaceshipService.Update(dto);
+
+            if (result == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- ShopTARpe25/ShopTARpe25/Models/Spaceship/SpaceshipUpdateViewModel.cs
+++ ShopTARpe25/ShopTARpe25/Models/Spaceship/SpaceshipUpdateViewModel.cs
@@ -2,6 +2,14 @@
 {
     public class SpaceshipUpdateViewModel
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Classification { get; set; } = string.Empty;
+        public DateTime? BuiltDate { get; set; }
+        public int? Crew { get; set; }
+        public int? EnginePower { get; set; }
 
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
